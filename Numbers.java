cae86394fe0618c2a9fa7d0aaf2062699d9f8dbb
chthomps <67965487+chthomps@users.noreply.github.com>
2021-07-28 13:39:03 -0500

    Update Numbers.java

--- Numbers.java
+++ Numbers.java
@@ -3,7 +3,7 @@
 public class Numbers {
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  //main method
 		
 		int[] numbers = {78, 22, 56, 99, 12, 14, 17, 15, 1, 144, 37, 23, 47, 88, 3, 19}; //array called numbers 
 		nextLargest(numbers);   //invoke nextLargest method on numbers array	
@@ -16,11 +16,11 @@ public class Numbers {
 		final int NUMVALUES =16;
 			
 		for (int index =0; index <NUMVALUES; index++) { //iterates through the array
-			int readPosition = numbers[index];    // variable advancing read position in the array
-			int nextLargest = Integer.MAX_VALUE;  // creating a variable set to the largest integer
+			int readPosition = numbers[index];    // variable setting/advancing readPosition in the array
+			int nextLargest = Integer.MAX_VALUE;  // resetting variable as the largest integer 
 			
 			for (int i =0; i <NUMVALUES; i++) { //iterates a particular readPosition against each other value in array 
-				if (numbers[i] > readPosition && numbers[i] <nextLargest) { //setting nextLargest to the next largest array number
+				if (numbers[i] > readPosition && numbers[i] <nextLargest) { //setting nextLargest to the next largest array number (stepping down from MAX_VALUE)
 					nextLargest = numbers[i];								
 				}	
 			
